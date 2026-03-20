354fa76920a05fad17289012db299b702563acb6
Bongo <kekoa5254@gmail.com>
2026-03-20 15:30:01 -0700

    Minor Changes

--- Dataset_Feature_Selection/feature_selection.cpp
+++ Dataset_Feature_Selection/feature_selection.cpp
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-#include "header/Search.hpp"
-#include "header/Puzzle.hpp"
-#include "header/Heuristics.hpp"
-#include "header/globals.hpp"
-
-#include <algorithm>
-#include <iostream>
-#include <unordered_set>
-#include <chrono>
-=======
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -19,14 +8,11 @@
 #include <limits>
 #include <chrono>
 #include <iomanip>
->>>>>>> Dataset_Feature_Selection/main
 
 using namespace std;
 
 /* ////////////////////////////////////////////////////////////////////
 
-<<<<<<< HEAD
-=======
 SCAN FILE FOR FEATURES/INSTANCES
 
 //////////////////////////////////////////////////////////////////// */
@@ -74,13 +60,10 @@ bool scanData(string fileName) {
 
 /* ////////////////////////////////////////////////////////////////////
 
->>>>>>> Dataset_Feature_Selection/main
 HELPERS
 
 //////////////////////////////////////////////////////////////////// */
 
-<<<<<<< HEAD
-=======
 // precompute squared distances for each feature
 void precomputeDistances() {
     int n = dataSet.size();
@@ -97,7 +80,6 @@ void precomputeDistances() {
     }
 }
 
->>>>>>> Dataset_Feature_Selection/main
 // user input checker
 int selectOptionHelper(int min, int max) {
     string input;
@@ -106,11 +88,7 @@ int selectOptionHelper(int min, int max) {
         cin >> input;
         cout << endl;
         try {
-<<<<<<< HEAD
-            number = stoi(input); // if fails, throws exception to catch
-=======
             number = stoi(input); 
->>>>>>> Dataset_Feature_Selection/main
         } catch(...) {
             cout << "INVALID OPTION. TRY AGAIN: ";
             continue;
@@ -123,20 +101,6 @@ int selectOptionHelper(int min, int max) {
     }
 }
 
-<<<<<<< HEAD
-// depth input checker
-vector<vector<int>> depthSelectHelper(int choice) {
-    vector<int> depthVals = {0,2,4,8,12,16,20,24};
-
-    auto it = find(depthVals.begin(), depthVals.end(), choice);
-    if(it == depthVals.end()){
-        cout << "Invalid depth.\n";
-        exit(1);
-    }
-
-    int index = distance(depthVals.begin(), it);
-    return depths[index];
-=======
 // Euclidean Distance formula helper
 double euclideanDistance(int row1, int row2, const vector<int>& featureSet, double bestSoFar) {
     double sum = 0.0;
@@ -346,7 +310,6 @@ void backwardElimination(ofstream& outFile) {
     outFile << "Finished search. Best feature subset is ";
     printSet(bestSet, outFile);
     outFile << " with accuracy " << bestAccuracy * 100 << "%" << endl;
->>>>>>> Dataset_Feature_Selection/main
 }
 
 /* ////////////////////////////////////////////////////////////////////
@@ -356,81 +319,6 @@ MAIN
 //////////////////////////////////////////////////////////////////// */
 
 int main() {
-<<<<<<< HEAD
-    border();
-    cout << "8-Puzzle Solver\n\n";
-    
-    // initial user prompt
-    cout << "SELECT OPTION: \n" <<
-            "(1) Default Puzzle\n" <<
-            "(2) Custom Puzzle\n";
-    int choice = selectOptionHelper(1, 2);
-    vector<vector<int>> puzzle(3, vector<int>(3));
-
-    if(choice == 1) { // default puzzle
-        cout << "ENTER DEPTH OF PUZZLE (0,2,4,8,12,16,20,24): ";
-
-        puzzle = depthSelectHelper(selectOptionHelper(0, 24));
-    }
-    else if(choice == 2) { // custom puzzle
-        unordered_set<int> seen;
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                int tile;
-                while(true) {
-                    cout << "Enter tile Number (Row " << i + 1 << ", Column " << j + 1 << ") [0-8, no duplicates]: ";
-                    cin >> tile;
-
-                    // check valid number
-                    if(tile < 0 || tile > 8) {
-                        cout << "INVALID: Must be between 0 and 8.\n";
-                        continue;
-                    }
-
-                    // check for duplicates
-                    if(seen.count(tile)) {
-                        cout << "INVALID: Tile already used.\n";
-                        continue;
-                    }
-
-                    // valid input
-                    puzzle[i][j] = tile;
-                    seen.insert(tile);
-                    break;
-                }
-            }
-        }
-    }
-    else {
-        cout << "ERROR";
-        return 0;
-    }
-
-    border();
-
-    cout << "Selected puzzle: \n";
-    displayPuzzle(puzzle, 1);
-
-    // algorithm choice prompt
-    cout << "SELECT ALGORITHM: \n" <<
-            "(1) Uniform Cost Search\n" <<
-            "(2) Misplaced Tile Heuristic (A*)\n" <<
-            "(3) Manhattan Distance Heuristic (A*)\n";
-    choice = selectOptionHelper(1, 3);
-    
-    border();
-
-    auto start = chrono::high_resolution_clock::now();
-    generalSearch(puzzle, choice);
-    auto end = chrono::high_resolution_clock::now();
-
-    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-    cout << "Execution Time: " << duration << " ms\n";
-
-    return 0;
-}
-
-=======
     cout << "Feature Selection Algorithm" << endl << endl;
     string fileName;
 
@@ -486,4 +374,3 @@ int main() {
 
     return 0;
 }
->>>>>>> Dataset_Feature_Selection/main
